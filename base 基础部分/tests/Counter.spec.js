8041f525e3ead65d3c5b11d09fbd950b6bdf958a
TTiip <2711430287@qq.com>
2021-10-25 23:03:10 +0800

    jest 钩子函数 准备工作 例如初始化变量等操作一定放到钩子函数中

--- base 基础部分/tests/Counter.spec.js
+++ base 基础部分/tests/Counter.spec.js
@@ -5,27 +5,27 @@ let counter = null
 describe('Counter的测试代码', () => {
 	// 所有测试用例执行之前
 	beforeAll(() => {
-		// console.log('beforeAll')
+		console.log('beforeAll')
 	})
 
 	// 每个测试用例执行之前 执行
 	beforeEach(() => {
-		// console.log('beforeEach')
+		console.log('beforeEach')
 		counter = new Counter()
 	})
 
 	// 所有测试用例执行之后
 	afterAll(() => {
-		// console.log('afterAll')
+		console.log('afterAll')
 	})
 	describe('测试增加相关的代码', () => {
 		test('测试 Counter中的 addOne', () => {
-			// console.log('addOne')
+			console.log('addOne')
 			counter.addOne()
 			expect(counter.number).toBe(1)
 		})
 		test('测试 Counter中的 addTwo', () => {
-			// console.log('addTwo')
+			console.log('addTwo')
 			counter.addTwo()
 			expect(counter.number).toBe(2)
 		})
@@ -33,13 +33,13 @@ describe('Counter的测试代码', () => {
 
 	describe('测试减少相关的代码', () => {
 		test('测试 Counter中的 minusOne', () => {
-			// console.log('minusOne')
+			console.log('minusOne')
 			counter.minusOne()
 			expect(counter.number).toBe(-1)
 		})
 
 		test('测试 Counter中的 minusTwo', () => {
-			// console.log('minusTwo')
+			console.log('minusTwo')
 			counter.minusTwo()
 			expect(counter.number).toBe(-2)
 		})
