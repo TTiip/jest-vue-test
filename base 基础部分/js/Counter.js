5b0911591269cda5d16d3f5c424c1cf34b052dba
TTiip <2711430287@qq.com>
2021-10-25 22:44:05 +0800

    jest 中的钩子函数

---
+++ base 基础部分/js/Counter.js
@@ -0,0 +1,23 @@
+class Counter {
+	constructor () {
+		this.number = 0
+	}
+
+	addOne () {
+		this.number += 1
+	}
+
+	minusOne () {
+		this.number -= 1
+	}
+
+	addTwo () {
+		this.number += 2
+	}
+
+	minusTwo () {
+		this.number -= 2
+	}
+}
+
+export default Counter
---
+++ base 基础部分/tests/Counter.spec.js
@@ -0,0 +1,47 @@
+import Counter from '../js/Counter'
+
+let counter = null
+
+describe('Counter的测试代码', () => {
+	// 所有测试用例执行之前
+	beforeAll(() => {
+		// console.log('beforeAll')
+	})
+
+	// 每个测试用例执行之前 执行
+	beforeEach(() => {
+		// console.log('beforeEach')
+		counter = new Counter()
+	})
+
+	// 所有测试用例执行之后
+	afterAll(() => {
+		// console.log('afterAll')
+	})
+	describe('测试增加相关的代码', () => {
+		test('测试 Counter中的 addOne', () => {
+			// console.log('addOne')
+			counter.addOne()
+			expect(counter.number).toBe(1)
+		})
+		test('测试 Counter中的 addTwo', () => {
+			// console.log('addTwo')
+			counter.addTwo()
+			expect(counter.number).toBe(2)
+		})
+	})
+
+	describe('测试减少相关的代码', () => {
+		test('测试 Counter中的 minusOne', () => {
+			// console.log('minusOne')
+			counter.minusOne()
+			expect(counter.number).toBe(-1)
+		})
+
+		test('测试 Counter中的 minusTwo', () => {
+			// console.log('minusTwo')
+			counter.minusTwo()
+			expect(counter.number).toBe(-2)
+		})
+	})
+})
