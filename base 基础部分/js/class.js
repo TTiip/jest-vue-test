69246530c60283abe0f9080c99eb3f886c0d73a8
TTiip <2711430287@qq.com>
2021-10-28 17:16:17 +0800

    对class的测试

---
+++ base 基础部分/js/class.js
@@ -0,0 +1,15 @@
+class Util {
+	init () {
+		// 特别复杂...
+	}
+
+	a () {
+		// 特别复杂...
+	}
+
+	b() {
+		// 特别复杂...
+	}
+}
+
+export default Util
---
+++ base 基础部分/tests/classFunc.spec.js
@@ -0,0 +1,15 @@
+import demoFunction from '../js/classFunc'
+import Util from '../js/class'
+
+// jest.mock 发现 Util 是一个类，会自动把类的构造函数和方法变成 jest.fn()
+jest.mock('../js/class')
+
+describe('classFunc.js', () => {
+	test('测试 demoFunction', () => {
+		demoFunction()
+		expect(Util).toHaveBeenCalled()
+		console.log(Util.mock.instances[0].a)
+		expect(Util.mock.instances[0].a).toHaveBeenCalled()
+		expect(Util.mock.instances[0].b).toHaveBeenCalled()
+	})
+})
